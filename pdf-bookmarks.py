6a1382470bd9e7181ccdec96cdda6cddf8cc6d1e
Michael Amie <michael.amie@gmail.com>
2014-05-23 22:47:55 -0700

    Forgot to close open files before; added 'with' block

--- pdf-bookmarks.py
+++ pdf-bookmarks.py
@@ -15,18 +15,19 @@ if __name__ == '__main__':
     pdf_path = ap.parse_args().pdf
     bookmarks_path = ap.parse_args().bookmarks
     page_offset = ap.parse_args().offset
-    
-    pdf = open(pdf_path, 'rb')
-    
-    output_pdf = PdfFileMerger()
-    output_pdf.merge(position=0, fileobj=pdf)
 
-    bookmarks = json.load(open(bookmarks_path))
-    
+    output_pdf = PdfFileMerger()
+
+    with open(pdf_path, 'rb') as pdf_file:
+        output_pdf.merge(position=0, fileobj=pdf_file)
+
+    with open(bookmarks_path, 'r') as bookmarks_file:
+        bookmarks = json.load(bookmarks_file)
     for name, page in bookmarks:
         output_pdf.addBookmark(name, page + int(page_offset) - 2)
     
     output_pdf_path = os.path.splitext(pdf_path)[0] + '-bookmarked.pdf'
-    output_pdf.write(open(output_pdf_path, 'wb'))
+    with open(output_pdf_path, 'wb') as pdf_file:
+        output_pdf.write(pdf_file)
 
     
